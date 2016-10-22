93869492686ed8e7bcc5cdd93bcbae831c5caec1
yel <yelmor@outlook.com>
2016-10-22 12:11:48 +0800

    fix:  add getter and setter for solution

--- errorcant-common/src/main/java/com/errorcant/model/Solution.java
+++ errorcant-common/src/main/java/com/errorcant/model/Solution.java
@@ -37,4 +37,68 @@ public class Solution {
 
   @LastModifiedDate
   private Date updateDate;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getErrorletId() {
+    return errorletId;
+  }
+
+  public void setErrorletId(String errorletId) {
+    this.errorletId = errorletId;
+  }
+
+  public String getLanguage() {
+    return language;
+  }
+
+  public void setLanguage(String language) {
+    this.language = language;
+  }
+
+  public String getCausation() {
+    return causation;
+  }
+
+  public void setCausation(String causation) {
+    this.causation = causation;
+  }
+
+  public String getSolution() {
+    return solution;
+  }
+
+  public void setSolution(String solution) {
+    this.solution = solution;
+  }
+
+  public List<String> getReferences() {
+    return references;
+  }
+
+  public void setReferences(List<String> references) {
+    this.references = references;
+  }
+
+  public Date getCreateDate() {
+    return createDate;
+  }
+
+  public void setCreateDate(Date createDate) {
+    this.createDate = createDate;
+  }
+
+  public Date getUpdateDate() {
+    return updateDate;
+  }
+
+  public void setUpdateDate(Date updateDate) {
+    this.updateDate = updateDate;
+  }
 }
